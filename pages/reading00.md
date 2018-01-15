ee5bed669ecb08c87eeaa3d836cde6c9ef8b3a8e
catvajiac <cvajiac@nd.edu>
2018-01-15 00:21:52 -0500

    restructure

---
+++ pages/reading00.md
@@ -0,0 +1,8 @@
+###Reading00
+
+TODO: answer the following questions:
+
+1. Why do people play games?
+2. What were your favorite (non-video) games to play growing up?
+3. What are the elements of an entertaining or engaging (non-video) game?
+4. In what ways are board games similar to video games? dissimilar? inferior? superior?
---
+++ scripts/build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import markdown
+import os
+import pygments
+
+if __name__ == "__main__":
+  posts = "../pages"
+  with open("../pages/skeleton.html", 'r') as fread:
+    line = fread.readline().strip()
+    while "</body>" not in line:
+      print(line)
+      line = fread.readline().strip()
+
+  # append blogposts
+  for file in os.listdir(posts):
+    if file[-3:] != ".md":
+      continue
+  
+    name, ext = file.split(".")
+
+    with open("../pages/{}".format(file), 'r') as fread:
+      for line in fread:
+        print(markdown.markdown(line))
+
+    print("</body>\n")
+    print("</html>")
